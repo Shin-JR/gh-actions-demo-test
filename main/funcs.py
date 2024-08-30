a297712366d606cc99ae60ee1c04670e86afe6db
paldev0 <jonathan.riquelme@paltech.eu>
2024-08-30 09:41:40 +0200

    feat: add github actions workflow "hello world"

---
+++ main/funcs.py
@@ -0,0 +1,8 @@
+def sum_2_digits(a: int | float, b: int | float) -> int | float:
+    return a + b
+
+def sum_multiple(*args: int | float) -> int | float:
+    return sum(args)
+
+def division(dividend: int | float, divisor: int | float) -> float:
+    return dividend / divisor
---
+++ tests/test_funcs.py
@@ -0,0 +1,21 @@
+from main.funcs import division, sum_2_digits, sum_multiple
+
+
+def test_sum_2_digits():
+    assert sum_2_digits(0, 0) == 0
+    assert sum_2_digits(0, 1) == 1
+    assert sum_2_digits(-2, 5) == 3
+    assert sum_2_digits(999999, 123456) == 1123455
+
+
+def test_sum_multiple():
+    assert sum_multiple(1, 2, 3, 4) == 10
+    assert sum_multiple(1, 2, 3, 4, 5, 6) == 21
+    assert sum_multiple(1, 2, 3, 4, 5, 6, 7) == 28
+    assert sum_multiple(1, 2, 3, 4, 5, 6, 7, 8) == 36
+
+
+def test_division():
+    assert division(4, 2) == 2
+    assert division(6, 3) == 2
+    assert division(18, 3) == 6
